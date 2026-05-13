cefa01c9136d92be5e2d6e7d55733d9d94351f58
pedrooberlaender01 <pedrooberlaender01@gmail.com>
2026-05-13 13:19:58 -0300

    rename: base path repo allanpg-captura-leads-insta

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Allan-PG---Captura-Leads---Insta/',
+  base: '/allanpg-captura-leads-insta/',
   plugins: [react()],
   server: { host: true, port: 5173 }
 });
